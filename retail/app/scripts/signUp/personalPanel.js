1904bc341a2d3d971941f2bb2ad6cd9e1a2dfe7d
Liu Yang <zation1@gmail.com>
2014-05-28 23:06:36 +0800

    correct tax id and ssn

--- retail/app/scripts/signUp/personalPanel.js
+++ retail/app/scripts/signUp/personalPanel.js
@@ -8,6 +8,14 @@ angular
       scope: {
         account: '=',
         submitted: '='
-      }
+      },
+      controller: ['$scope', function($scope) {
+        $scope.isUseSSN = true;
+        $scope.$watch('account.socialSecurityNumber', function(SSN) {
+          if ($scope.isUseSSN) {
+            $scope.account.taxId = SSN;
+          }
+        });
+      }]
     };
   }]);
