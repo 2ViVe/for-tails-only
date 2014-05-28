cd18e88e86d835bc64b985eaa9e22c74c6796f05
Liu Yang <zation1@gmail.com>
2014-05-28 12:25:13 +0800

    add urlhandler

--- retail/app/scripts/initialize.js
+++ retail/app/scripts/initialize.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('fto')
-  .constant('DEFAULT_COUNTRY_ID', 1214)
-  .run(['$rootScope', 'cfpLoadingBar', '$location',
-    function($rootScope, cfpLoadingBar, $location) {
+  .constant({
+    'DEFAULT_COUNTRY_ID': 1214,
+    'PORT_FOR_NON_SECURE_RETAIL_DEMO_SITE': 11442,
+    'PORT_FOR_SECURE_RETAIL_DEMO_SITE': 22442,
+    'PORT_FOR_BACK_OFFICE_DEMO_SITE': 22442,
+    'URL_FOR_RETAIL_PRODUCTION_SITE': 'http://www.fortailsonly.com',
+    'URL_FOR_BACK_OFFICE_PRODUCTION_SITE': 'https://backoffice.fortailsonly.com',
+    'URL_FOR_RETAIL_DEMO_SITE': 'fto.www',
+    'URL_FOR_BACK_OFFICE_DEMO_SITE': 'fto.backoffice',
+    'DEFAULT_ROLE_CODE': 'R'
+  })
+  .run(['$rootScope', 'cfpLoadingBar', '$location', 'UrlHandler',
+    function($rootScope, cfpLoadingBar, $location, UrlHandler) {
       $rootScope.$on('$routeChangeStart', function() {
         cfpLoadingBar.start();
+      });
+
+      $rootScope.$on('$locationChangeStart', function(event) {
+        UrlHandler.handleSecurityPath(function() {
+          event.preventDefault();
+        });
       });
 
       $rootScope.$on('$routeChangeError', function() {
