47dc820fa991c3911ac94bd986b72765e795488a
Liu Yang <zation1@gmail.com>
2014-05-30 16:54:13 +0800

    add default use home address

--- retail/app/scripts/signUp/addressPanelCtrl.js
+++ retail/app/scripts/signUp/addressPanelCtrl.js
@@ -4,7 +4,6 @@ angular
   .module('fto/signup')
   .controller('AddressPanelCtrl', ['$scope', 'Registration.Countries', function($scope, Countries) {
 
-
     if ($scope.addressType !== 'website') {
       Countries
         .fetch()
@@ -14,20 +13,13 @@ angular
         });
     }
 
-    $scope.useHomeAddress = function() {
-      $scope.address.firstName = $scope.homeAddress.firstName;
-      $scope.address.m = $scope.homeAddress.m;
-      $scope.address.lastName = $scope.homeAddress.lastName;
-      $scope.address.phone = $scope.homeAddress.phone;
+    $scope.isUseHomeAddress = $scope.defaultUseHomeAddress ? true : false;
 
-      if ($scope.addressType !== 'website') {
-        $scope.address.street = $scope.homeAddress.street;
-        $scope.address.streetCont = $scope.homeAddress.streetCont;
-        $scope.address.city = $scope.homeAddress.city;
-        $scope.address.state = $scope.homeAddress.state;
-        $scope.address.country = $scope.homeAddress.country;
-        $scope.address.zip = $scope.homeAddress.zip;
-        $scope.address.phone = $scope.homeAddress.phone;
+    $scope.isUseHomeAddressChange = function(isUseHomeAddress) {
+      if (isUseHomeAddress) {
+        $scope.address.extendDataFrom($scope.homeAddress);
+      } else {
+        $scope.address.cleanData();
       }
     };
   }]);
