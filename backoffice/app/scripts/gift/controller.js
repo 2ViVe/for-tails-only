dbe3bc011c42dc31934e33f2f75461c6976c8a64
Liu Yang <zation1@gmail.com>
2014-06-01 23:19:37 +0800

    how amount and images for gift card

--- backoffice/app/scripts/gift/controller.js
+++ backoffice/app/scripts/gift/controller.js
@@ -1,8 +1,16 @@
 'use strict';
 
 angular.module('fto/gift')
-  .controller('GiftController', ['$scope', '$modal',
-    function($scope, $modal) {
+  .controller('GiftController', ['$scope', '$modal', 'giftCard',
+    function($scope, $modal, giftCard) {
+      $scope.giftCards = giftCard.data;
+      $scope.currentTheme = giftCard.data[0];
+      $scope.currentGiftCard = '';
+
+      $scope.changeTheme = function(theme) {
+        $scope.currentTheme = theme;
+      };
+
       $scope.preview = function() {
         $modal.open({
           templateUrl: '/views/gift/preview.html',
