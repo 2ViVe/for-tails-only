5639ad35fae0f1e83f35f3e596b09cb653bb6d07
kiddkai <kiddkai@gmail.com>
2014-06-07 06:53:46 +0800

    zekai -profile - shows correct address information

--- retail/app/scripts/profile/profileAddressPanel.js
+++ retail/app/scripts/profile/profileAddressPanel.js
@@ -39,56 +39,6 @@
           });
       };
 
-      $scope.getCountryName = function(countryId) {
-        if (!$scope.countries) {
-          return '';
-        }
-        angular.forEach($scope.countries, function(country) {
-          if (country.id === countryId) {
-            $scope.address.country = country.name;
-            return;
-          }
-        });
-        return $scope.address.country;
-      };
-
-      $scope.getStates = function(selectedCountryId) {
-        angular.forEach($scope.countries, function(country) {
-          if (country.id === selectedCountryId) {
-            $scope.states = country.states;
-            return;
-          }
-        });
-        return $scope.states;
-      };
-
-      $scope.getCountryById = function(countryId) {
-        var country;
-        if (!$scope.countries) {
-          return null;
-        }
-        angular.forEach($scope.countries, function(c) {
-          if (c.id === countryId) {
-            country = c;
-            return;
-          }
-        });
-        return country;
-      };
-
-      $scope.getStateName = function(countryId, stateId) {
-        var result = '';
-        if (!$scope.countries) { return ''; }
-        if (!$scope.getStates(countryId)) { return ''; }
-        angular.forEach($scope.getStates(countryId), function(state) {
-          if (state.id === stateId) {
-            result = state.name;
-          }
-        });
-        return result;
-      };
-
-
     }])
     .directive('profileAddressPanel', function() {
       return {
