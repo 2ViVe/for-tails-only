6dc48f0c9b4eee3af0faad6b5e7fdf6c103c829e
Liu Yang <zation1@gmail.com>
2014-06-08 00:41:10 +0800

    modify URL_FOR_RETAIL_DEMO_SITE and URL_FOR_BACK_OFFICE_DEMO_SITE

--- backoffice/app/scripts/initialize.js
+++ backoffice/app/scripts/initialize.js
@@ -8,8 +8,8 @@ angular.module('fto')
     'PORT_FOR_BACK_OFFICE_DEMO_SITE': 22442,
     'URL_FOR_RETAIL_PRODUCTION_SITE': 'http://www.fortailsonly.com',
     'URL_FOR_BACK_OFFICE_PRODUCTION_SITE': 'https://backoffice.fortailsonly.com',
-    'URL_FOR_RETAIL_DEMO_SITE': 'fto.www',
-    'URL_FOR_BACK_OFFICE_DEMO_SITE': 'fto.backoffice',
+    'URL_FOR_RETAIL_DEMO_SITE': 'www',
+    'URL_FOR_BACK_OFFICE_DEMO_SITE': 'backoffice',
     'DEFAULT_ROLE_CODE': 'R',
     'CURRENCY_SYMBOL': '$'
   })
