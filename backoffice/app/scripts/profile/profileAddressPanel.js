39dc50ea382b15242de6ef8d1946cc6de7024397
kiddkai <kiddkai@gmail.com>
2014-06-07 12:41:21 +0800

    zekai - update backend profile again - sync with retail

--- backoffice/app/scripts/profile/profileAddressPanel.js
+++ backoffice/app/scripts/profile/profileAddressPanel.js
@@ -3,6 +3,23 @@
 (function() {
 
   angular.module('2ViVe')
+    .directive('datePicker', function() {
+      return {
+        restrict: 'C',
+        link: function(scope, element) {
+          var maxDate = moment().add(-18, 'years');
+          var minDate = moment().add(-150, 'years');
+
+          new Pikaday({
+            field: angular.element(element)[0],
+            yearRange: [minDate.year(), maxDate.year()],
+            defaultDate: moment().add(-30, 'years').toDate(),
+            minDate: minDate.toDate(),
+            maxDate: maxDate.toDate()
+          });
+        }
+      };
+    })
     .controller('profileAddressPanelCtrl', ['$scope', 'Address', 'Registration', 'Registration.Countries', function($scope, Address, Registration, Countries) {
 
       $scope.isEditing = false;
@@ -13,6 +30,9 @@
         Address
           .fetch()
           .then(function(addr) {
+            if (!addr[$scope.addressType.toLowerCase()]) {
+              addr.addType($scope.addressType.toLowerCase());
+            }
             $scope.address = addr[$scope.addressType.toLowerCase()];
             $scope.initAddress = angular.copy($scope.address);
           });
