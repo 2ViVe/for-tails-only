b7d02f75041628c3084d93eedaf72cba5523b970
Liu Yang <zation1@gmail.com>
2014-06-13 14:07:32 +0800

    display rsvp without email

--- retail/app/scripts/party/app.js
+++ retail/app/scripts/party/app.js
@@ -15,10 +15,21 @@ angular
         templateUrl: 'views/party/invitation.html',
         controller: 'PartyInvitationController',
         resolve: {
-          'event': ['Event', '$route',
-            function(Event, $route) {
-              var event = new Event($route.current.params.eventId);
-              return event.fetch();
+          'event': ['Event', '$route', '$q',
+            function(Event, $route, $q) {
+              var deferred = $q.defer();
+
+              var id = $route.current.params.eventId;
+              var event = new Event(id);
+              $q.all([event.fetch(), event.fetchInvitees()])
+                .then(function() {
+                  deferred.resolve(event);
+                })
+                .catch(function(error) {
+                  deferred.reject(error);
+                });
+
+              return deferred.promise;
             }]
         }
       });
