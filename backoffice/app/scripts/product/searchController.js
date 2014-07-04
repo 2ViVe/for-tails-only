af496db518dc221ee1e97e0043d509d4ec645716
Liu Yang <zation1@gmail.com>
2014-07-04 17:04:35 +0800

    add pagination for search products

--- backoffice/app/scripts/product/searchController.js
+++ backoffice/app/scripts/product/searchController.js
@@ -1,8 +1,18 @@
 'use strict';
 
 angular.module('fto/product')
-  .controller('SearchController', ['$scope', 'data', '$route',
-    function($scope, data, $route) {
-      $scope.products = data.products;
+  .controller('SearchController', ['$scope', 'data', '$route', 'Products',
+    function($scope, data, $route, Products) {
+      $scope.fetchData = function(page) {
+        Products.fetch({
+          q: $scope.query,
+          offset: (page - 1) * $scope.limit
+        }).then(function(data) {
+          $scope.data = data;
+        });
+      };
+
+      $scope.data = data;
       $scope.query = $route.current.params.query;
+      $scope.limit = 20;
     }]);
