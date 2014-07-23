c13d0ccb16e3cbaebcaa5d06aa66a5a0de97856c
Liu Yang <zation1@gmail.com>
2014-07-23 21:32:54 +0800

    add cancel function

--- retail/app/scripts/autoship/controller.js
+++ retail/app/scripts/autoship/controller.js
@@ -2,10 +2,19 @@
 
 angular
   .module('fto/autoship')
-  .controller('AutoShipsController', ['$scope', 'autoShips', '$modal',
-    function($scope, autoShips, $modal) {
+  .controller('AutoShipsController', ['$scope', 'autoShips', '$modal', 'AutoShip',
+    function($scope, autoShips, $modal, AutoShip) {
 
       $scope.autoShips = autoShips;
+
+      $scope.cancel = function(autoShip) {
+        var _autoShip = new AutoShip();
+        _autoShip.id = autoShip.id;
+        _autoShip.cancel()
+          .then(function() {
+            autoShip.state = 'cancelled';
+          });
+      };
 
       $scope.showDetails = function(autoShip) {
         $modal.open({
