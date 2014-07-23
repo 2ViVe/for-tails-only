5fb8cec42644d2eb93e87298ee7be3b4421b3a6f
Liu Yang <zation1@gmail.com>
2014-07-23 15:08:32 +0800

    happy pass autoship create

--- retail/app/scripts/autoship/checkoutController.js
+++ retail/app/scripts/autoship/checkoutController.js
@@ -2,10 +2,27 @@
 
 angular
   .module('fto/autoship')
-  .controller('AutoShipCheckoutController', ['$scope', 'autoShip',
-    function($scope, autoShip) {
+  .controller('AutoShipCheckoutController', ['$scope', 'autoShip', '$modalInstance', 'paymentMethods',
+    function($scope, autoShip, $modalInstance, paymentMethods) {
 
       $scope.order = autoShip.orderSummary;
+      $scope.paymentMethods = paymentMethods;
+
+      $scope.autoShip = autoShip;
+      $scope.autoShip.creditcard = {};
+      $scope.autoShip.paymentMethod = paymentMethods[0];
+
+      $scope.checkout = function() {
+        $scope.submitted = true;
+
+        if (this.paymentForm.$valid) {
+          autoShip.create();
+        }
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
 
     }
   ]
--- retail/app/scripts/autoship/createController.js
+++ retail/app/scripts/autoship/createController.js
@@ -5,19 +5,21 @@ angular
   .controller('AutoShipCreateController', ['$scope', 'autoShips', 'address', '$modal', 'AutoShip',
     function($scope, autoShips, address, $modal, AutoShip) {
 
-      autoShips.fetchShippingMethods(address.shipping.country.id, address.shipping.state.id)
-        .then(function() {
-          $scope.shippingMethods = autoShips.shippingMethods;
-          $scope.shippingMethod = $scope.shippingMethods[0];
-        });
-
-      $scope.address = address;
-      $scope.products = autoShips.products;
-      $scope.autoshipDay = 7;
-      $scope.startDate = {
+      var autoShip = new AutoShip();
+      autoShip.address = address;
+      autoShip.autoShipDay = 7;
+      autoShip.startDate = {
         month: 1,
         year: 2014
       };
+      $scope.autoShip = autoShip;
+      autoShips.fetchShippingMethods(address.shipping.country.id, address.shipping.state.id)
+        .then(function() {
+          $scope.shippingMethods = autoShips.shippingMethods;
+          $scope.autoShip.shippingMethod = $scope.shippingMethods[0];
+        });
+
+      $scope.products = autoShips.products;
 
       $scope.submit = function() {
         $scope.error = null;
@@ -26,13 +28,18 @@ angular
           templateUrl: 'views/auto-ship/checkout.html',
           controller: 'AutoShipCheckoutController',
           resolve: {
+            paymentMethods: function() {
+              return autoShips.fetchPaymentMethods(autoShip.address.shipping.country.id)
+                .then(function(autoShips) {
+                  return autoShips.paymentMethods;
+                });
+            },
             products: function() {
               return $scope.products;
             },
             autoShip: ['$q', function($q) {
               var deferred = $q.defer();
 
-              var autoShip = new AutoShip();
               var autoShipItems = [];
               angular.forEach($scope.products, function(product) {
                 angular.forEach(product.variants, function(variant) {
@@ -44,10 +51,8 @@ angular
                   }
                 });
               });
-              autoShip.address = $scope.address;
-              autoShip.orderSummary(autoShipItems,
-                $scope.address.shipping,
-                $scope.shippingMethod.id)
+              autoShip.autoShipItems = autoShipItems;
+              autoShip.orderSummary()
                 .then(function() {
                   deferred.resolve(autoShip);
                 })
