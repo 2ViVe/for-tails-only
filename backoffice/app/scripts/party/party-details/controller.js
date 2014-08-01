8db3126e8457e6df4f55bbaadb52f98273bcd977
Liu Yang <zation1@gmail.com>
2014-08-01 15:24:01 +0800

    add Half Price Items and change the layout

--- backoffice/app/scripts/party/party-details/controller.js
+++ backoffice/app/scripts/party/party-details/controller.js
@@ -9,20 +9,27 @@ angular.module('fto/party')
       $scope.invitees = event.invitees;
 
       $scope.rewords = 0;
+      $scope.halfPriceItems = 0;
       $scope.rewordsPercentage = undefined;
       var ordersItemTotal = event.ordersItemTotal();
       if (ordersItemTotal >= 200 && ordersItemTotal < 299) {
         $scope.rewords = 15;
+        $scope.halfPriceItems = 1;
       } else if (ordersItemTotal >= 300 && ordersItemTotal < 399) {
         $scope.rewords = 25;
+        $scope.halfPriceItems = 2;
       } else if (ordersItemTotal >= 400 && ordersItemTotal < 499) {
         $scope.rewords = 45;
+        $scope.halfPriceItems = 3;
       } else if (ordersItemTotal >= 500 && ordersItemTotal < 799) {
         $scope.rewords = 60;
+        $scope.halfPriceItems = 4;
       } else if (ordersItemTotal >= 800 && ordersItemTotal < 999) {
         $scope.rewords = 90;
+        $scope.halfPriceItems = 5;
       } else if (ordersItemTotal >= 1000) {
         $scope.rewords = 115;
+        $scope.halfPriceItems = 6;
       }
 
       $scope.deleteInvites = function() {
